a591fa9ff639bfd74ddab2be8279b99eb59909bf
jimjam197 <Sjc97@live.com>
2023-10-01 16:23:41 -0400

    ticket 3 complete

--- chat-client/src/components/display/room/DeleteRoom.jsx
+++ chat-client/src/components/display/room/DeleteRoom.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Container, Button } from "reactstrap";
 
-function DeleteRoom() {
-    return (
-        <div>DeleteRoom</div>
-    )
+function DeleteRoom(props) {
+  const handleDeleteRoom = async () => {
+  };
+
+  return (
+    <Container
+      fluid
+      className="justify-content"
+      style={{
+        background: "grey",
+        height: "85vh",
+        width: "70vw",
+        marginTop: "1em",
+        marginBottom: "2em",
+        padding: "1em",
+      }}
+    >
+      <Row className="gx-3">
+        <Col md="2">
+          <Button onClick={handleDeleteRoom}>Delete Room</Button>
+        </Col>
+        <Col md="10">
+          <h3>{props.selectedRoom.title}</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default DeleteRoom
+export default DeleteRoom;
--- chat-client/src/components/display/room/RoomDisplay.jsx
+++ chat-client/src/components/display/room/RoomDisplay.jsx
@@ -1,81 +1,74 @@
-import React from 'react'
-import { Container, Form, Input, Label, Row, Col, Button } from 'reactstrap'
+import React from "react";
+import {
+  Container,
+  Form,
+  Input,
+  Label,
+  Row,
+  Col,
+  Button,
+  Sidebar,
+} from "reactstrap";
+import UpdateRoom from "./UpdateRoom";
+import DeleteRoom from "./DeleteRoom";
 
-function RoomDisplay( props ) {
+function RoomDisplay(props) {
+  const handleUpdateRoom = async () => {
+  };
 
+  const handleDeleteRoom = async () => {
+  };
 
-
-    return (
-        <>
-            <Container fluid className='justify-content'
-                style={
-                    {
-                        background: 'grey',
-                        height: '85vh',
-                        width: '70vw',
-                        marginTop: '1em',
-                        marginBottom: '2em',
-                        padding: '1em'
-
-                    }
-                }>
-
-
-                <Row className='gx-3'>
-                    <Col md='2'>
-                        <Button>Delete</Button>
-                    </Col>
-                    <Col md='10'>
-                        <h3>{props.selectedRoom.title}</h3>
-                    </Col>
-                </Row>
-                <Row className='mt-3'>
-                    <Col md='2'>
-                        <h3>Description</h3>
-                        <p>{props.selectedRoom.description}</p>
-                    </Col>
-                    <Col>
-                        <Container
-                            style={
-                                {
-                                    background: 'white',
-                                    height: '40rem',
-                                    width: '65rem'
-                                }
-                            }
-                        >
-
-                        </Container>
-                    </Col>
-                </Row>
-                <Row className='mt-3'>
-                    <Col md='2'>
-                        <Button>Update Room</Button>
-                    </Col>
-                    <Col md='10'>
-                        <Form>
-                            <Row className='align-items-center'>
-                                <Col md='10'>
-                                    <Input
-                                        type='text'
-                                        name='message'
-                                        id='message'
-                                    />
-                                </Col>
-                                <Col md='2'>
-                                    <Button>
-                                        Submit
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </Col>
-                </Row>
-
-            </Container>
-
-        </>
-    )
+  return (
+    <>
+      <Sidebar>
+        <UpdateRoom
+          open={props.openUpdateRoom}
+          handleUpdateRoom={handleUpdateRoom}
+        />
+        <DeleteRoom
+          open={props.openDeleteRoom}
+          handleDeleteRoom={handleDeleteRoom}
+        />
+      </Sidebar>
+      <Container
+        fluid
+        className="justify-content"
+        style={{
+          background: "grey",
+          height: "85vh",
+          width: "70vw",
+          marginTop: "1em",
+          marginBottom: "2em",
+          padding: "1em",
+        }}
+      >
+        <Row className="gx-3">
+          <Col md="2">
+            <Button onClick={() => props.openUpdateRoom()}>Update Room</Button>
+          </Col>
+          <Col md="10">
+            <h3>{props.selectedRoom.title}</h3>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col md="2">
+            <h3>Description</h3>
+            <p>{props.selectedRoom.description}</p>
+          </Col>
+          <Col>
+            <Container
+              style={{
+                background: "white",
+                height: "40rem",
+                width: "65rem",
+              }}
+            ></Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default RoomDisplay
+export default RoomDisplay;
