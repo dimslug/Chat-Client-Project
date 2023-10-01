28b1397ec90e7bb1221746fc6c32770d3d725e3a
jimjam197 <Sjc97@live.com>
2023-10-01 16:35:27 -0400

    Ticket 4 done

--- chat-client/src/components/display/message/DeleteMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DeleteMessage() {
-    return (
-        <div>DeleteMessage</div>
-    )
-}
-
-export default DeleteMessage
--- chat-client/src/components/display/message/UpdateMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UpdateMessage() {
-    return (
-        <div>UpdateMessage</div>
-    )
-}
-
-export default UpdateMessage
